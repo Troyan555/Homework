6c19d2f03dd0338b317b86c0a012ecda1b5094d3
Troyan555 <113114745+Troyan555@users.noreply.github.com>
2023-01-25 23:08:44 +0200

    Hw15 (#9)
    * hw15 add

---
+++ scr/hw15/QuickSort.java
@@ -0,0 +1,52 @@
+package hw15;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void quickSort(int[] array, int low, int high) {
+        if (array.length == 0)
+            return;
+
+        if (low >= high)
+            return;
+
+        int middle = low + (high - low) / 2;
+        int prop = array[middle];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < prop) {
+                i++;
+            }
+            while (array[j] > prop) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+    }
+    public static void main(String[] args) {
+        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
+        System.out.println("Was");
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        quickSort(x, low, high);
+        System.out.println("Became");
+        System.out.println(Arrays.toString(x));
+    }
+}
+
