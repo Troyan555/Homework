1a4bc237610ef221e2fda60039e2ed09753ba0fb
irinatroian <irina.troian.89@gmail.com>
2023-04-28 13:37:29 +0300

    HW add

---
+++ scr/hw13/Main.java
@@ -0,0 +1,9 @@
+package hw13;
+
+public class Main {
+    public static void main(String[] args) {
+        ValueCalculator valueCalculator = new ValueCalculator();
+        valueCalculator.doSomethingWithMultithreading();
+        valueCalculator.doSomethingWithoutMultithreading();
+    }
+}
