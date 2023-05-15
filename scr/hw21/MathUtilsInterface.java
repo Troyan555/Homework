ad4f9b5a2151b80e5e5c0e13e58ad39a56bd5772
irinatroian <irina.troian.89@gmail.com>
2023-05-15 20:22:47 +0300

    HW added

---
+++ scr/hw21/MathUtilsInterface.java
@@ -0,0 +1,8 @@
+package hw21;
+
+public interface MathUtilsInterface {
+    int add (int a, int b);
+    int substract (int a, int b);
+    int multiply (int a, int b);
+    double divide (double a, double b);
+}
